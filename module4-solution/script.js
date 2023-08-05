9c60fff890d502dbc59ed0b97595246d271136e9
gamermat2002 <shibilsmathew27@gmail.com>
2023-08-05 18:08:55 +0530

    Assignment 4
    -

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -2,9 +2,9 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 
 for(var i=0;i<names.length;i++){
     if(names[i].charAt(0)=='J' || names[i].charAt(0)=='j'){
-        console.log(byeSpeaker.speak()+names[i]);
+        console.log("Good Bye "+names[i]);
     }
     else{
-        console.log(helloSpeaker.speak()+names[i]);
+        console.log("Hello "+names[i]);
     }
 }
